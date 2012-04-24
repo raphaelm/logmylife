1c162d0b0bbe37adb69d84b1d97ec6755a4795da
Raphael Michel <webmaster@raphaelmichel.de>
2012-04-24 13:05:39 +0200

    Do not crash because of one exception

--- generate.py
+++ generate.py
@@ -3,6 +3,8 @@ import os
 import time
 import datetime
 from optparse import OptionParser
+import traceback
+import sys
 
 import lml.base
 
@@ -19,11 +21,15 @@ class Generator(lml.base.Generator):
 		USER = "_rami_"
 		FILE = "data/%s.twitter.sqlite.db" % USER
 		FILEPREFIX = "charts/%s.twitter.chart." % USER
-		if download:
-			lml.twitter.Download(FILE, USER).download()
-		chart = lml.twitter.Charts(FILE, FILEPREFIX, USER).create()
-		self.charts.append((chart, "Twitter: _rami_"))
-		print "Twitter done."
+		try:
+			if download:
+				lml.twitter.Download(FILE, USER).download()
+			chart = lml.twitter.Charts(FILE, FILEPREFIX, USER).create()
+			self.charts.append((chart, "Twitter: _rami_"))
+			print "Twitter done."
+		except Exception as e:
+			traceback.print_exc(file=sys.stdout)
+			print "Twitter error."
 
 	def mail(self, scan = True):
 		print "Mail"
@@ -33,11 +39,15 @@ class Generator(lml.base.Generator):
 		FILEPREFIX = "charts/%s.maildir.chart." % ACCOUNT
 		ME = "(raphaelmichel95@(googlemail|gmail).com|.+@raphaelmichel.de|michel@schlusen.net|rami@daquel.de|raphaelm@php.net|raphael@geeksfactory.de)"
 		MDIR = os.path.expanduser("~/backup/gmail/")
-		if scan:
-			lml.maildir.Scan(MDIR, FILE).scan()
-		chart = lml.maildir.Charts(FILE, FILEPREFIX, ME, ACCOUNT).create()
-		self.charts.append((chart, "E-Mail: raphaelmichel95@gmail.com"))
-		print "Mail done."
+		try:
+			if scan:
+				lml.maildir.Scan(MDIR, FILE).scan()
+			chart = lml.maildir.Charts(FILE, FILEPREFIX, ME, ACCOUNT).create()
+			self.charts.append((chart, "E-Mail: raphaelmichel95@gmail.com"))
+			print "Mail done."
+		except Exception as e:
+			traceback.print_exc(file=sys.stdout)
+			print "Mail error."
 
 	def lastfm(self, download = True):
 		print "Music"
@@ -47,33 +57,45 @@ class Generator(lml.base.Generator):
 		FILEPREFIX = "charts/%s.lastfm.chart." % USER
 		APIK = "b8258575158335e66482df0777e5b331"
 		OFFSET = time.mktime(datetime.datetime(2010,01,01).timetuple())
-		if download:
-			lml.music.DownloadLastFm(FILE, USER, APIK, OFFSET).download()
-		chart = lml.music.Charts(FILE, FILEPREFIX, USER).create()
-		self.charts.append((chart, "Music (Last.fm): rami95"))
-		print "Music done."
+		try:
+			if download:
+				lml.music.DownloadLastFm(FILE, USER, APIK, OFFSET).download()
+			chart = lml.music.Charts(FILE, FILEPREFIX, USER).create()
+			self.charts.append((chart, "Music (Last.fm): rami95"))
+			print "Music done."
+		except Exception as e:
+			traceback.print_exc(file=sys.stdout)
+			print "Music error."
 		
 	def ping(self, download = True):
 		print "Ping"
 		import lml.ping
 		KEYHASH = "0e0f17629059a77b7d4b0a6d95845e5b7a1c4490" # SHA1 hash of my "ping" key
-		SERVER = "http://www.raphaelmichel.de/stats/pingserver/data/"
+		SERVER = "http://www.raphaelmichel.de/stats/pingserver/"
 		
 		DEVICE = "PC"
 		FILE = "data/%s.ping.raw.txt" % DEVICE
 		FILEPREFIX = "charts/%s.ping.chart." % DEVICE
-		if download:
-			lml.ping.Download(FILE, KEYHASH, DEVICE).download()
-		chart = lml.ping.Charts(FILE, FILEPREFIX, DEVICE).create()
-		self.charts.append((chart, "Online time: desktop computer"))
+		try:
+			if download:
+				lml.ping.Download(FILE, SERVER, KEYHASH, DEVICE).download()
+			chart = lml.ping.Charts(FILE, FILEPREFIX, DEVICE).create()
+			self.charts.append((chart, "Online time: desktop computer"))
+		except Exception as e:
+			traceback.print_exc(file=sys.stdout)
+			print "Ping error."
 		
 		DEVICE = "android"
 		FILE = "data/%s.ping.raw.txt" % DEVICE
 		FILEPREFIX = "charts/%s.ping.chart." % DEVICE
-		if download:
-			lml.ping.Download(FILE, KEYHASH, DEVICE).download()
-		chart = lml.ping.Charts(FILE, FILEPREFIX, DEVICE).create()
-		self.charts.append((chart, "Online time: smartphone"))
+		try:
+			if download:
+				lml.ping.Download(FILE, SERVER, KEYHASH, DEVICE).download()
+			chart = lml.ping.Charts(FILE, FILEPREFIX, DEVICE).create()
+			self.charts.append((chart, "Online time: smartphone"))
+		except Exception as e:
+			traceback.print_exc(file=sys.stdout)
+			print "Ping error."
 		
 		print "Ping done."
 		
